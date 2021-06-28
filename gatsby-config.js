fcf7a44dcd8f653442befba60d09de55beea277c
Zakir Hossain <zakirhossain@dhcp-130-58-96-238.swarthmore.edu>
2021-06-28 16:49:51 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "obf",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
