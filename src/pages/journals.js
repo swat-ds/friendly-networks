7bffe92e6c7f96a1a7716e302e98497711809f78
Zakir Hossain <zakirhossain@dhcp-130-58-96-238.swarthmore.edu>
2021-07-22 23:18:36 -0400

    reorganized some of the links

--- src/pages/journals.js
+++ src/pages/journals.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 
 import { Link, graphql } from "gatsby";
 import styled from "styled-components";
 import Layout from "../components/Layout";
 import Image from "../components/Image";
-import {Button} from 'react-bootstrap'
+import { Button } from "react-bootstrap";
 
-const journals = ({data}) => {
-    return (
-      <>
-        <Layout>
-          <Button variant="outline-info">
-            <Link to={"/" + data.allCetei.nodes[0].parent.name}>
-              John Hunt
-            </Link>
-          </Button>
-        </Layout>
-      </>
-    );
-}
+const journals = ({ data }) => {
+  return (
+    <>
+      <Layout>
+        <Button variant="outline-info">
+          <Link to={"/" + data.allCetei.nodes[0].parent.name}>Journal 1</Link>
+        </Button>
+      </Layout>
+    </>
+  );
+};
 
 export const data = graphql`
   {
@@ -35,5 +33,4 @@ export const data = graphql`
     }
   }
 `;
-export default journals
-
+export default journals;
