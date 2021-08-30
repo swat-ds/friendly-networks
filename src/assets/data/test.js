8f3d8a36e1ca5f8831eee18cffc0012325ff230c
Zakir Hossain <zakirhossain@dhcp-130-58-96-238.swarthmore.edu>
2021-08-30 15:49:02 -0400

    Sorted entities alphabetically with the name Sorted journals with the starting year Honed some of the aesthetics of network. More to be done

--- src/assets/data/test.js
+++ src/assets/data/test.js
@@ -7,3 +7,18 @@ const obj = {
 };
 
 console.log(obj)
+
+// Create Objects Array
+
+var arrayCarObjects = [
+{brand: "Honda",        topSpeed: 45},
+{brand: "Ford",         topSpeed: 6},
+{brand: "Toyota",       topSpeed: 240},
+{brand: "Chevrolet",    topSpeed: 120},
+{brand: "Ferrari",      topSpeed: 1000}
+];
+
+
+arrayCarObjects.sort((a, b) => (a.brand > b.brand) ? 1 : -1);
+
+console.log(arrayCarObjects);
