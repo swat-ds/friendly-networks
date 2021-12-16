98401768447a52f96463ff6b29d897b35630b0f2
James Truitt <jtruitt1@swarthmore.edu>
2021-12-16 11:10:49 -0500

    Update homepage
    Add text acknowledging David & Joyce Hunt Foundation

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,13 @@ import "../styles/styles.scss";
 const home = ({location}) => {
   return (
     <Layout>
-      <Row>
-        <h1 className="general-text">Our Beloved Friend: The Journals of John Hunt</h1>
-        <p className="general-text">
+      <Row className="general-text">
+        <h1>Our Beloved Friend: The Journals of John Hunt</h1>
+        <p>
         Since its origins in the 1650s, the Quaker community has encompassed a densely interconnected transatlantic network. Friendly Networks explores ways to describe Quaker identities, outline Quaker social networks, and connect them to archival sources, with a focus on the journals of New Jersey minister John Hunt (1740-1824). These journals, kept from 1770 to 1824, document not only Hunt’s daily activities and religious life but also his advocacy for non-violence, the end of slavery, and the fair treatment of Native Americans and African Americans.
+        </p>
+        <p>
+        This website and the research behind it are made possible with support from the H. David and Joyce E. Hunt Family Foundation Special Projects Fund.
         </p>
       </Row>
     </Layout>
