f9683cc367a6f53ab0b11ce8a79f76adebaca817
James Truitt <jtruitt1@swarthmore.edu>
2021-12-19 13:35:04 -0500

    Create gatsby-browser.js

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+/**
+* Implement Gatsby's Browser APIs in this file.
+*
+* See: https://www.gatsbyjs.com/docs/browser-apis/
+*/
+// You can delete this file if you're not using it
+import "bootstrap/dist/css/bootstrap.min.css" // This to be added.
