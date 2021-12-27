a8d0a015020c527307ce49627768506cf4cd9a93
James Truitt <jtruitt1@swarthmore.edu>
2021-12-26 21:22:40 -0500

    Redo cards on bg pages to match new format

--- src/pages/background/author-bg.js
+++ src/pages/background/author-bg.js
@@ -2,9 +2,19 @@ import React from "react";
 import { Link } from "gatsby";
 import Layout from "../../components/Layout";
 import { globalVariables } from "../../globalVariables";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import "../../styles/pageStyles.scss";
+import homepageCards from "../../components/HomepageCards"
 
+// Image imports
+import one from "../../../content/markdown/images/image1.jpg";
+import two from "../../../content/markdown/images/image2.jpg";
+import three from "../../../content/markdown/images/image3.jpg";
+import four from "../../../content/markdown/images/image4.jpg";
+import five from "../../../content/markdown/images/image5.jpg";
+import six from "../../../content/markdown/images/image6.jpg";
+import nine from "../../../content/markdown/images/image9.jpg";
+import ten from "../../../content/markdown/images/image10.jpg";
 
   //  timeline: "/background/author-background/timeline",
   //  bookshelf: "/background/john-hunt/7-bookshelf",
@@ -15,92 +25,87 @@ import "../../styles/pageStyles.scss";
   //  bibliography: "/background/author-background/bibliography",
 
 const author_bg = () => {
-  return (
-    <Layout>
-      <Row id="main-row">
-        <Col id="bg-col">
-          <h2>Backgrounds of John Hunt</h2>
-          <Link className="g-link2" to={globalVariables.timeline}>
-            <div className="box cyan">
-              <h2>Timeline</h2>
-              <p>
-                A timeline of events occurred in John Hunt and his families
-                lives, both visual and text formats
-              </p>
-            </div>
-            {/* <Button variant="outline-success">Timeline</Button> */}
-          </Link>
-          <Link className="g-link2" to={globalVariables.bibliography}>
-            {/* <Button variant="outline-success">Bibliography</Button> */}
-            <div className="box red">
-              <h2>Bibliography</h2>
-              <p>Bibliography for the reference of this essay</p>
-            </div>
-          </Link>
-          <Link className="g-link2" to={globalVariables.quaker_world}>
-            {/* <Button variant="outline-success">World of John Hunt</Button> */}
-            <div className="box blue">
-              <h2>World of John Hunt</h2>
-              <p>
-                Brief descriptive summary of how John Hunt lived is life,
-                including his daily routines, practices, and other involvements{" "}
-              </p>
-            </div>
-          </Link>
-          <Link className="g-link2" to={globalVariables.family}>
-            {/* <Button variant="outline-success">Family</Button> */}
-            <div className="box orange">
-              <h2>John Hunt's Family</h2>
-              <p>
-                Descriptions and charts of John Hunt's immediate and extended
-                family members
-              </p>
-            </div>
-          </Link>
-          <Link className="g-link2" to={globalVariables.bookshelf}>
-            {/* <Button variant="outline-success">Bookshelf</Button> */}
-            <div className="box cyan">
-              <h2>John Hunt's Bookshelf</h2>
-              <p>
-                Which texts had John Hunt read and incorporated as he wrote his
-                journals
-              </p>
-            </div>
-          </Link>
-          <Link className="g-link2" to={globalVariables.mentors_role_models}>
-            {/* <Button variant="outline-success">Friends</Button> */}
-            <div className="box red">
-              <h2>Dear Friends</h2>
-              <p>
-                Some of the people John Hunt venerated, lived among, and
-                exchanged dialogues with
-              </p>
-            </div>
-          </Link>
-          <Link className="g-link2" to={globalVariables.whyKeep}>
-            {/* <Button variant="outline-success">Friends</Button> */}
-            <div className="box blue">
-              <h2>Why Keep the journals</h2>
-              <p>
-                Why keep a journal? Hunt explained his resolve in the very first
-                entry...
-              </p>
-            </div>
-          </Link>
-          <Link className="g-link2" to="/background/author-bg/all">
-            {/* <Button variant="outline-success">All in One</Button> */}
-            <div className="box blue">
-              <h2>The Entire Essay</h2>
-              <p>
-                Click on this one to view the entire essay, timeline,
-                bibliography, etc.{" "}
-              </p>
-            </div>
-          </Link>
-        </Col>
-      </Row>
-    </Layout>
-  );
+    const pageCardData = [
+        {
+            text: "Introducing John Hunt",
+            link: "/background/john-hunt/1-introducing-john-hunt",
+            imageSrc: four,
+            alt: "",
+            id: ""
+        },
+        {
+            text: "The Form of the Journal",
+            link: "/background/quaker-bg/0-Form_of_the_Journal",
+            imageSrc: six,
+            alt: "",
+            id: ""
+        },
+        {
+            text: "“Advantages”: Why Keep a Journal?",
+            link: "/background/john-hunt/2-why-keep-a-journal",
+            imageSrc: "",
+            alt: "",
+            id: ""
+        },
+        {
+            text: "“About Home:” The Hunt Family",
+            link: "/background/john-hunt/3-family",
+            imageSrc: ten,
+            alt: "",
+            id: ""
+        },
+        {
+            text: "The Quaker World of John Hunt",
+            link: "/background/john-hunt/4-quaker-world",
+            imageSrc: one,
+            alt: "",
+            id: ""
+        },
+        {
+            text: "“Dear Friends:” Mentors and Role Models",
+            link: "/background/john-hunt/5-mentors-role-models",
+            imageSrc: two,
+            alt: "",
+            id: ""
+        },
+        {
+            text: "“Everything that is Required of Us:” Hunt & Quaker Testimonies",
+            link: "/background/john-hunt/6-quaker-testimonies",
+            imageSrc: nine,
+            alt: "",
+            id: ""
+        },
+        {
+            text: "Hunt’s Bookshelf",
+            link: "/background/john-hunt/7-bookshelf",
+            imageSrc: "",
+            alt: "",
+            id: ""
+        },
+        {
+            text: "Chronology of Hunt's Life",
+            link: "/background/timeline",
+            imageSrc: "",
+            alt: "",
+            id: ""
+        },
+        {
+            text: "John Hunt material at Friends Historical Library",
+            link: "/background/john-hunt/8-Hunt-bibliography",
+            imageSrc: "",
+            alt: "",
+            id: ""
+        }
+    ]
+
+    return (
+        <Layout>
+            <Row>
+                <h1>Background: John Hunt</h1>
+                {homepageCards(pageCardData)}
+            </Row>
+        </Layout>
+    );
 };
 
 export default author_bg;
--- src/pages/background/quaker-bg.js
+++ src/pages/background/quaker-bg.js
@@ -3,17 +3,48 @@ import Layout from "../../components/Layout";
 import { globalVariables } from "../../globalVariables";
 import {Row, Col} from 'react-bootstrap';
 import {Link} from 'gatsby';
+import homepageCards from "../../components/HomepageCards"
 // import "../../../assets/styles/pageStyles.scss";
 // import { Row, Col } from "react-bootstrap";
 // const appendixData = require("../../../assets/data/john_hunt_appendix.json");
 
+// Image imports
+import three from "../../../content/markdown/images/image3.jpg";
+import five from "../../../content/markdown/images/image5.jpg";
 
-const quakeer_bg = () => {
+const pageCardData = [
+    {
+        text: "Structure of Quaker Meetings",
+        link: "/background/quaker-bg/structure-society-friends",
+        imageSrc: three,
+        alt: "",
+        id: ""
+    },
+    {
+        text: "Glossary of Quaker terms",
+        link: "/background/quaker-bg/glossary",
+        imageSrc: five,
+        alt: "",
+        id: ""
+    }
+]
+
+const quaker_bg = () => {
   // const term = (term) => (
   //   <p className="background-text">
   //     <strong>{term.term}</strong>: {term.definition}
   //   </p>
   // );
+
+  return(
+      <Layout>
+        <h1>Background: The Society of Friends</h1>
+        <Row>
+            {homepageCards(pageCardData)}
+        </Row>
+      </Layout>
+  );
+
   return (
     <Layout>
       <Row id="main-row">
@@ -56,4 +87,4 @@ const quakeer_bg = () => {
   );
 };
 
-export default quakeer_bg;
+export default quaker_bg;
