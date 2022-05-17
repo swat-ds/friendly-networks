f69df92a32c8a0deaad3aca1cebc15ba3732279f
James Truitt <jtruitt1@swarthmore.edu>
2022-05-17 13:59:15 -0400

    Add ability to pass GeoJSON data to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,27 +2,38 @@ import React from "react";
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
-import { Marker, Popup } from 'react-leaflet'
+import { Marker, Popup, GeoJSON } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 const position = [39.856677,-74.90081]
 
-const Map = ({position}) => {
+const Map = ({center, maxZoom, minZoom, startZoom, json}) => {
+
+  // Create function for onEachFeature
+  // (this extracts the name from a feature to display in a pop-up)
+  const addPopup = (feature, layer) => {
+    console.log("In addMarker");
+    if (feature.properties && feature.properties.name) {
+      console.log(feature.properties.name);
+      layer.bindPopup(`<p>${feature.properties.name}</p>`);
+    }
+  }
   return (
-    <MapContainer center={position} zoom={7} scrollWheelZoom={true} style={{ height: "600px" }}>
+    <MapContainer center={center} zoom={startZoom} scrollWheelZoom={true} style={{ height: "600px" }}>
       <TileLayer
         attribution='Tiles &copy; Esri &mdash; Source: Esri'
         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}.png"
-        maxZoom="11"
-        minZoom="5"
+        maxZoom={maxZoom}
+        minZoom={minZoom}
       />
       <TileLayer
         attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.png"
-        maxZoom="11"
-        minZoom="5"
+        maxZoom={maxZoom}
+        minZoom={minZoom}
       />
-      <Marker position={position}>
+      <GeoJSON data={json} onEachFeature={addPopup}/>
+      <Marker position={center}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
--- src/pages/mapTest.js
+++ src/pages/mapTest.js
@@ -6,13 +6,61 @@ import Map from "../components/Map"
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 
+
+const jsonData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type":"Feature",
+      "geometry": {
+        "type":"Point",
+        "coordinates": [-75.0376700, 39.8915000]
+      },
+      "properties": {
+        "name": "Haddonfield",
+        "administrationCode": "NJ",
+        "countryCode": "US"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry": {
+        "type":"Point",
+        "coordinates": [-86.2502700, 35.7503500]
+      },
+      "properties": {
+        "name": "Tennessee",
+        "administrationCode": "TN",
+        "countryCode": "US"
+      }
+    },
+    {
+      "type":"Feature",
+      "geometry": {
+        "type":"Point",
+        "coordinates": [-74.6682000, 39.8776900]
+      },
+      "properties": {
+        "administrationCode": "NJ",
+        "countryCode": "US"
+      }
+    }
+  ]
+}
+
 const MapPage = ({ data }) => {
   return (
     <Layout>
       <Row>
         <Col/>
         <Col>
-          <Map position={[39.856677,-74.90081]}/>
+          <Map
+            center={[39.856677,-74.90081]}
+            maxZoom={11}
+            minZoom={5}
+            startZoom={7}
+            json={jsonData}
+          />
         </Col>
       </Row>
     </Layout>
