397d83bc462c34e9128b7619a90b7eb65dac06ef
James Truitt <jtruitt1@swarthmore.edu>
2025-12-17 09:26:09 -0500

    Stop /writings/A00187320 from failing to build
    Get the manifest from [islandora]/[nodeId]/manifest-SINGLE for our only single-page item

--- src/components/Volume.js
+++ src/components/Volume.js
@@ -60,6 +60,9 @@ async function fetchAsync(url) {
  */
 function getImageUrls(manifest) {
   const iiifCanvases = manifest.sequences[0].canvases
+  if (! iiifCanvases) {
+    return null // Handle this downstack by calling up …/manifest-single
+  }
   const urls = iiifCanvases.map(canvas => canvas.images[0].resource.service["@id"])
   return urls
 }
@@ -164,7 +167,9 @@ let counter = 0; // counter to track the index of each transcript (cetei)
     const imageUrls = useMemo(() => {
       const imageUrls = []
       const nodeId = getNodeId(jsonPrefixed)
-      const url = `https://digitalcollections.tricolib.brynmawr.edu/node/${nodeId}/manifest`
+      const url = nodeId === '488797' 
+        ? `https://digitalcollections.tricolib.brynmawr.edu/node/${nodeId}/manifest-single`
+        : `https://digitalcollections.tricolib.brynmawr.edu/node/${nodeId}/manifest`;
       const manifest = fetchAsync(url) // Get IIIF presentation manifest
       manifest.then(data => imageUrls.push(...getImageUrls(data)))
         .then(() => setFetched(true))
