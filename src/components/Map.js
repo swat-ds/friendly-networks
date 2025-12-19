6fa2dc2d79cbaaa3834ce285649addf988ca806a
James Truitt <jtruitt1@swarthmore.edu>
2025-12-19 10:22:26 -0500

    Improve UX of Maps page
    - Add tabs to switch between instructions and background
    - Key descriptions of trip and links to journal pages to map selection buttons

--- src/components/Map.js
+++ src/components/Map.js
@@ -122,7 +122,7 @@ const StartButton = (hasPath) => {
       className="leaflet-control leaflet-bar" 
       onClick={ () => toMarker(0, refs, map)}
     >
-      Go to Start
+      Beginning
     </Button>
   )
 }
--- src/pages/maps.js
+++ src/pages/maps.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 import { Link } from "gatsby"
 
-import { Row, Col, ToggleButtonGroup, ToggleButton } from "react-bootstrap";
+import { Row, Col, Tab, Tabs, ToggleButtonGroup, ToggleButton } from "react-bootstrap";
 
 import Layout from "../components/Layout";
 import Map from "../components/Map"
@@ -16,12 +16,33 @@ import { redman } from "/content/geodata/Redman.js"
 
 const MapPage = ({ data }) => {
   const maps = [
-    {name: "Evans's 1st journey North", data: nj1},
-    {name: "Evans's 2nd journey North", data: nj2},
-    {name: "Evans's 1st journey South", data: sj1},
-    {name: "Evans's 2nd journey South", data: sj2},
-    {name: "Redman's journey North", data: redman},
+    {
+      name: "Evans's 1794 journey North",
+      data: nj1,
+      jsx: <>Joshua Evans&rsquo;s travels in New England, 1794 (<Link to="/writings/A0011541#page001">first page</Link>, <Link to="/writings/A0011541#page068"> summary tables</Link>)</>
+    },
+    {
+      name: "Evans's 1795 journey North",
+      data: nj2,
+      jsx: <>Joshua Evans&rsquo;s travels in New York and Canada, 1795 (<Link to="/writings/A0011541#page039">first page</Link>, <Link to="/writings/A0011541#page072">summary tables</Link>)</>
+    },
+    {
+      name: "Evans's 1796–97 journey South",
+      data: sj1,
+      jsx: <>Joshua Evans&rsquo;s travels in the US Southeast, 1796&ndash;1797 (<Link to="/writings/A0011543#page001">first page</Link>, <Link to="/writings/A0011543#page065">summary tables</Link>)</>
+    },
+    {
+      name: "Evans's 1798 journey South",
+      data: sj2,
+      jsx: <>Joshua Evans&rsquo;s travels in Virginia &amp; Pennsylvania, 1798 (<Link to="/writings/A0011543#page073">first page</Link>, <Link to="/writings/A0011543#page075">summary tables</Link>)</>
+    },
+    {
+      name: "Redman's 1760 journey North",
+      data: redman,
+      jsx: <>Mercy Redman&rsquo;s travels in New England, 1760 (<Link to="/writings/A00187301">first page</Link>)</>
+    },
   ]
+  console.log(maps[0]);
   
   const [currentMap, setMap] = useState(0)
 
@@ -57,30 +78,63 @@ const MapPage = ({ data }) => {
     <Layout>
       <Row id="main-row">
         <h1>Maps of Travels in the Ministry</h1>
-        <div id="instructions">
-          <p>
-            Use the green buttons below to choose which 
-            journey you would like to see mapped.
-          </p>
-          <p>
-            On the map, green lines marked with arrows trace the travel route.
-            Green markers note stops on the journey. 
-            Click one to see the name of the place, 
-            the date the traveler reached it, and more.
-          </p>
-          <p>
-            Follow along with the journey using the "Next" and "Prev"
-            buttons. The "To Start" button in the top left will take you
-            to the first stop.
-          </p>
-        </div>
-        <ToggleButtonGroup 
-            name="collection" 
-            type="radio"
-            defaultValue={currentMap}
-            id="map-choice"
-            vertical={filterVertical}
-          >
+        <Tabs defaultActiveKey="Instructions" id="map-text">
+          <Tab eventKey="Instructions" title="Instructions">
+            <p>
+              Use the green buttons below to choose which
+              journey you would like to see mapped.
+            </p>
+            <p>
+              On the map, green lines marked with arrows trace the travel route.
+              Green markers note stops on the journey.
+              Click one to see the name of the place,
+              the date the traveler reached it, and more.
+            </p>
+            <p>
+              Follow along with the journey by using the "Next" and "Prev"
+              buttons after opening a marker. The "Beginning" button in the top
+              right of the map will take you to the first stop.
+            </p>
+          </Tab>
+          <Tab eventKey="About" title="What is this?">
+            <p>
+              It has long been the custom of Friends for a spiritually weighty member
+              to undertake a journey to visit other congregations, a practice known
+              as “traveling in the ministry.” These journeys could last anywhere
+              from several weeks to several years, during which the traveling
+              minister would attend various meetings, often including the sessions of
+              one or more yearly meetings. Families who were members of a local
+              congregation might also meet with the traveling minister individually,
+              and sometimes additional sessions of a meeting were called or “appointed”
+              for the occasion of the visit. These visits were a key way of keeping
+              far-flung Friends connected to a wider Quaker community that spanned the
+              Atlantic World, as the travelers often brought news of friends and family
+              from distant meetings. In a religious society without a single central
+              authority, the interchange of ministers also helped keep Friends in step
+              on doctrine. These journeys thus served as important threads tying the whole
+              Friendly network of transatlantic Quakers together.
+            </p>
+            <p>
+              Many of the ministers featured in <i>Friendly Networks</i> traveled in the
+              ministry, and a number kept journals recording their journeys. Five of these
+              trips are highlighted on this page: Joshua Evans’s four journeys in the 1790s,
+              and Mercy Redman’s journey in 1760. To visualize these journeys, <Link to="/credits">
+                FHL staff and students</Link> tracked
+              down, to the best of their ability, the locations mentioned in
+              each journal. This was made easier in Evans’s case by the fact that he kept lists
+              of the locations he visited and the distances between them, which in many cases
+              helped narrow down ambiguous geographic terms.
+            </p>
+          </Tab>
+        </Tabs>
+
+        <ToggleButtonGroup
+          name="collection"
+          type="radio"
+          defaultValue={currentMap}
+          id="map-choice"
+          vertical={filterVertical}
+        >
           {maps.map((map, idx) => (
             <ToggleButton
               className="map-toggle-btn"
@@ -91,12 +145,15 @@ const MapPage = ({ data }) => {
               name="map"
               value={idx}
               checked={currentMap === idx}
-              onChange={(e) => setMap(e.currentTarget.value)}
+              onChange={(e) => {
+                setMap(e.currentTarget.value)
+              }}
             >
               {map.name}
             </ToggleButton>
           ))}
-          </ToggleButtonGroup>
+        </ToggleButtonGroup>
+        <p>{maps[currentMap].jsx}</p>
         <Col id="map-col">
           <Map
             maxZoom={11}
@@ -107,60 +164,6 @@ const MapPage = ({ data }) => {
             path={true}
           />
         </Col>
-        <div id="explanation">
-          <p>
-            It has long been the custom of Friends for a spiritually weighty member 
-            to undertake a journey to visit other congregations, a practice known 
-            as “traveling in the ministry.” These journeys could last anywhere 
-            from several weeks to several years, during which the traveling 
-            minister would attend various meetings, often including the sessions of 
-            one or more yearly meetings. Families who were members of a local 
-            congregation might also meet with the traveling minister individually, 
-            and sometimes additional sessions of a meeting were called or “appointed” 
-            for the occasion of the visit. These visits were a key way of keeping 
-            far-flung Friends connected to a wider Quaker community that spanned the 
-            Atlantic World, as the travelers often brought news of friends and family 
-            from distant meetings. In a religious society without a single central 
-            authority, the interchange of ministers also helped keep Friends in step 
-            on doctrine. These journeys thus served as important threads tying the whole 
-            Friendly network of transatlantic Quakers together. 
-          </p>
-          <p>
-            Many of the ministers featured in <i>Friendly Networks</i> traveled in the 
-            ministry, and a number kept journals recording their journeys. Five of these 
-            trips are highlighted on this page: Joshua Evans’s four journeys in the 1790s, 
-            and Mercy Redman’s journey in 1760. To visualize these journeys, <Link to="/credits">
-            FHL staff and students</Link> tracked
-            down, to the best of their ability, the locations mentioned in 
-            each journal. This was made easier in Evans’s case by the fact that he kept lists 
-            of the locations he visited and the distances between them, which in many cases 
-            helped narrow down ambiguous geographic terms. 
-          </p>
-          <p>The journeys thus mapped are:
-            <ul>
-              <li> 
-                Joshua Evans&rsquo;s travels in New England, 1794 
-                (<Link to="/writings/A0011541#page001">first page</Link>, <Link to="/writings/A0011541#page068">summary tables</Link>)
-              </li>
-              <li>
-                Joshua Evans&rsquo;s travels in New York and Canada, 1795 
-                (<Link to="/writings/A0011541#page039">first page</Link>, <Link to="/writings/A0011541#page072">summary tables</Link>)
-              </li>
-              <li>
-                Joshua Evans&rsquo;s travels in the US Southeast, 1796&ndash;1797 
-                (<Link to="/writings/A0011543#page001">first page</Link>, <Link to="/writings/A0011543#page065">summary tables</Link>)
-              </li>
-              <li>
-                Joshua Evans&rsquo;s travels in Virginia &amp; Pennsylvania, 1798 
-                (<Link to="/writings/A0011543#page073">first page</Link>, <Link to="/writings/A0011543#page075">summary tables</Link>)
-              </li>
-              <li>
-                Mercy Redman&rsquo;s travels in New England, 1760 
-                (<Link to="/writings/A00187301">first page</Link>)
-              </li>
-            </ul>
-          </p>
-        </div>
       </Row>
     </Layout>
   );
