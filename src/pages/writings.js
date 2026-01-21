9a0699923e2dd9acf755911c7bbf3672f38b9054
James Truitt <jtruitt1@swarthmore.edu>
2026-01-21 14:44:38 -0500

    Add page copy to Writings

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -145,37 +145,94 @@ const genres = [
     {
       name: "Hunt journals",
       collection: "John Hunt",
-      genre: "journal"
+      genre: "journal",
+      text: <>
+        The 24 journals of minister <Link to="people/w6n9820p">John  
+        Hunt</Link> are valuable sources for Quaker history in the late 
+        eighteenth and early nineteenth centuries. They record over 50 years 
+        of Hunt’s daily life in Burlington County, New Jersey (1770–1824, 
+        though little material has survived from the period 1800–1812). The 
+        journals document Hunt’s advocacy for non-violence, the abolition of 
+        slavery, and the fair treatment of Native Americans and 
+        African Americans.
+      </>
     },
     {
       name: "Evans journals",
       collection: "Evans",
-      genre: "journal"
+      genre: "journal",
+      text: <>
+        The three journals of Quaker minister <Link to="/people/w6c82qz0">Joshua 
+        Evans</Link> begin with an autobiography and go on to detail  
+        his <Link to='/maps'>religious travels</Link> as far as Nova Scotia 
+        and Georgia in the 1790s. The journals document Evans’s advocacy for 
+        non-violence, the abolition of slavery, and the fair treatment of 
+        Native Americans and African Americans.
+      </>
     },
     {
       name: "Hunt letters",
       collection: "John Hunt",
-      genre: "letter"
+      genre: "letter",
+      text: <>
+        Ten letters to and from <Link to="people/w6n9820p">John  
+        Hunt</Link> and others in his orbit illustrate the beliefs of other 
+        Friends in the network and help contextualize his journals.
+      </>
     },
     {
       name: "Evans letters",
       collection: "Evans",
-      genre: "letter"
+      genre: "letter",
+      text: <>
+        These 29 letters to and from <Link to="/people/w6c82qz0">Joshua 
+        Evans</Link> and his family, mostly sent to his wife while he  
+        was <Link to='/maps'>traveling in the ministry</Link>, demonstrate the 
+        reactions of other Friends to his ministry. 
+      </>
     },
     {
       name: "Redman journal",
       collection: "Redman",
-      genre: ""
+      genre: "",
+      text: <>
+        The journal of Quaker minister <Link to="/people/w6x098rs">Mercy 
+        Redman</Link> documents her <Link to="/maps">journey</Link> to New
+        York, Rhode Island, and Massachusetts, parts of which were undertaken 
+        with <Link to="/people/w6wr0v4m">John Woolman</Link>. 
+      </>
     },
     {
       name: "Collins letters",
       collection: "Roberts",
-      genre: ""
+      genre: "",
+      text: <>
+        <Link to="/people/w6gj3q0h">Esther Roberts Hunt Collins</Link>, a 
+        sister-in-law of John Hunt and elder of Redstone Meeting, was an early 
+        Quaker settler in western Pennsylvania. Her  
+        husband <Link to="/people/w6n05cnp">Joshua Hunt</Link> died shortly 
+        after the family reached Redstone, and Esther Hunt spent the next 15
+        years raising her six children alone. Her family’s 49 letters document 
+        this period, as well as her return to New Jersey after  
+        marrying <Link to="/people/w6165774">John Collins</Link> in 1807.
+      </>
     },
     {
       name: "Yarnall items",
       collection: "Yarnall",
-      genre: ""
+      genre: "",
+      text: <>
+        <Link to="/people/w6g77m31">Hannah Haines Thornton Yarnall</Link> 
+        was a Quaker minister, the widow of 
+        minister <Link to="/people/w6hf8m7k">Joseph Thornton</Link>, 
+        and the second wife of 
+        minister <Link to="/people/w6kx66c3">Peter Yarnall</Link>. 
+        An 1803 journal documents her religious travels to visit Quakers in 
+        Pelham and Newmarket, Ontario, Canada.  A set of 25 letters to and 
+        from Yarnall’s family gives insight into their daily lives and 
+        occasionally provides the perspectives of other ministers in their 
+        network.
+      </>
     },
     {
       name: "All",
@@ -189,29 +246,16 @@ const genres = [
        <Row id="main-row"><Col>
          <h1>Writings</h1>
          <p>
-            The writings of John Hunt and Joshua Evans are valuable sources for 
-            Quaker history in the late 18th and early 19th centuries. 
-            Hunt’s journals record over 50 years of his daily life in Burlington 
-            County, New Jersey (1770–1824). 
-            Evans’s journals detail his <Link to='/maps'>religious travels</Link>  
-            as far as Nova Scotia and Georgia in the 1790s. 
-            Both men’s journals document their advocacy for non-violence, the 
-            abolition of slavery, and the fair treatment of Native Americans 
-            and African Americans. 
+            To explore the writings of John Hunt, Joshua Evans, and their 
+            peers, choose a set of documents using the tabs below. Click a 
+            document's card to browse images and transcripts of that document. 
+            Documents are listed in chronological order within each category.
          </p>
-         <p>
-            Several other sets of documents help contextualize these journals. 
-            Letters to and from Hunt, Evans, and their families illustrate 
-            the beliefs of other Friends in their network and their 
-            reactions to Hunt and Evans’s ministries. 
-            In addition, journals and correspondence from several female 
-            ministers in Hunt and Evans’s network—Mercy Redman, Esther Hunt 
-            Collins, and Hannah Thornton Yarnall—offer women's perspectives on 
-            Quaker life in southern New Jersey and beyond.
-         </p>
+        
         <div id="tab-row"><Tabs id="document-tabs" fill>
           {documentSets.map(
             set => <Tab eventKey={set.name} title={set.name}>
+              <p style={{textWrap: 'pretty'}}>{set.text}</p>
               <Col id="document-card-col">
                 <Row xs={2} md={3} lg={4} xl={5} xxl={6} id="document-card-row">
                   {preparedNodes // Filter nodes and then render each one as card
